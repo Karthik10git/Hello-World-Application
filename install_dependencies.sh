9944bf1f5434906c92815e8295ab66a30a6b0b8a
Karthik <79147303+Karthik10git@users.noreply.github.com>
2022-08-25 10:24:11 +0530

    Delete install_dependencies.sh

--- install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get -y update
-apt-get -y install python3
-apt-get -y install python3-pip
-apt-get -y install nginx
-apt-get -y install gunicorn3
-pip3 install flask
-mkdir flaskapp
-cd flaskapp
